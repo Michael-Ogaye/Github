68d1405fb89ec27c51caa42b9c3413d0953dbf55
Michael Ogaye <hogaeymic@gmail.com>
2022-04-18 14:37:25 +0300

    env:configurations

--- src/app/services/gitservice.service.ts
+++ src/app/services/gitservice.service.ts
@@ -1,8 +1,9 @@
+import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
+import { catchError, retry } from 'rxjs/operators'
+
 
 interface userData{
   avatar_url:string;
